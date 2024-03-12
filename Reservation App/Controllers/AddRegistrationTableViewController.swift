b56f31d77bc32d73dbddc0b72bf29f0c96679dcc
Abdullah Arslan <abdullaharslan1473@gmail.com>
2024-03-12 10:57:22 +0300

    check In and check Out functions have been defined. Conditions were given

--- Reservation App/Controllers/AddRegistrationTableViewController.swift
+++ Reservation App/Controllers/AddRegistrationTableViewController.swift
@@ -12,20 +12,66 @@ class AddRegistrationTableViewController: UITableViewController {
     // MARK: - UI Elements
     @IBOutlet var firstNameTextField: UITextField!
     @IBOutlet var lastNameTextField: UITextField!
-    @IBOutlet var emailNameTextField: UITextField!
+    @IBOutlet var emailTextField: UITextField!
+    
+    @IBOutlet var checkInDateLabel: UILabel!
+    @IBOutlet var checkInDatePicker: UIDatePicker!
+    
+    @IBOutlet var checkOutDateLabel: UILabel!
+    
+    @IBOutlet var checkOutDatePicker: UIDatePicker!
     
     // MARK: - Preportties
+    let ONE_DAY: Double = 24 * 60 * 60
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        dateSettings()
     }
     
     // MARK: - Functions
     
+    func updateDateViews() {
+        
+      
+        checkOutDatePicker.minimumDate = checkInDatePicker.date.addingTimeInterval(ONE_DAY)
+        
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = .medium
+        
+        checkInDateLabel.text = dateFormatter.string(from: checkInDatePicker.date)
+        
+        checkOutDateLabel.text = dateFormatter.string(from: checkOutDatePicker.date)
+        
+    }
+    
+    func dateSettings() {
+        
+        let midnightToday = Calendar.current.startOfDay(for: Date())
+        
+        checkInDatePicker.minimumDate = midnightToday
+        checkInDatePicker.date = midnightToday
+                
+    }
+    
+    
+    
     
     // MARK: - Actions
+    @IBAction func doneButtonTapped(_ button: UIBarButtonItem)
+    {
+        let firstName = firstNameTextField.text!
+        let lastName = lastNameTextField.text!
+        let email = emailTextField.text!
+        
+    }
+    @IBAction func datePickerValueChanged(_ picker: UIDatePicker) {
+        updateDateViews()
+        
+    }
+    
 
 
     
